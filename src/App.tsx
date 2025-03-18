c34410f75e9f090bd6c2e018fd9d1c603c518037
jchyeon0814 <jchyeon0814@gmail.com>
2025-03-19 07:55:46 +0900

    카운터 화면 생성

--- src/App.tsx
+++ src/App.tsx
@@ -11,9 +11,48 @@ const Container = styled.div`
   justify-content: center;
 `;
 
+const Title = styled.h1`
+  margin-bottom: 32px;
+`;
+
+const Contents = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Label = styled.span`
+  margin: 0 16px;
+  font-size: 1.2rem;
+`;
+
+const Button = styled.button`
+  border: 0;
+  color: #ffffff;
+  background-color: #ff5722;
+  cursor: pointer;
+  padding: 8px 16px;
+  border-radius: 4px;
+
+  &:hover {
+    background-color: #ff5722;
+    opacity: 0.8;
+  }
+
+  &:active {
+    box-shadow: inset 5px 5px 10px rgba(0, 0, 0, 0.2);
+  }
+`;
+
 function App() {
   return (
     <Container>
+      <Title>Counter App</Title>
+      <Contents>
+        <Button>-</Button>
+        <Label>0</Label>
+        <Button>+</Button>
+      </Contents>
     </Container>
   );
 }
