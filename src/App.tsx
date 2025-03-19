1a2d092395b58697aa1531b4fb513f06e33e9fec
jchyeon0814 <jchyeon0814@gmail.com>
2025-03-20 07:54:53 +0900

    Button에 Props 적용하여 변경

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import styled from '@emotion/styled'
 import { useState } from 'react';
+import { Button } from 'components/Button';
 
 const Container = styled.div`
   height: 100vh;
@@ -27,23 +28,7 @@ const Label = styled.span`
   font-size: 1.2rem;
 `;
 
-const Button = styled.button`
-  border: 0;
-  color: #ffffff;
-  background-color: #ff5722;
-  cursor: pointer;
-  padding: 8px 16px;
-  border-radius: 4px;
 
-  &:hover {
-    background-color: #ff5722;
-    opacity: 0.8;
-  }
-
-  &:active {
-    box-shadow: inset 5px 5px 10px rgba(0, 0, 0, 0.2);
-  }
-`;
 
 function App() {
   const [counter, setCounter] = useState(0);
@@ -60,9 +45,9 @@ function App() {
     <Container>
       <Title>Counter App</Title>
       <Contents>
-        <Button onClick={sub}>-</Button>
+        <Button label="-" onClick={sub} />
         <Label>{counter}</Label>
-        <Button onClick={add}>+</Button>
+        <Button label="+" onClick={add} />
       </Contents>
     </Container>
   );
